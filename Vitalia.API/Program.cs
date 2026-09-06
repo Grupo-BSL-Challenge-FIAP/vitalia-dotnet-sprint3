8a80029a768192f30111f38a315b15a404d9a06f
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-05 22:01:49 -0300

    refactor(Program): alterado rota para Vitalia.Infrastructure.Data

--- Vitalia.API/Program.cs
+++ Vitalia.API/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Vitalia.API.Data;
+using Vitalia.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
--- Vitalia.Infrastructure/Data/VitaliaDbContext.cs
+++ Vitalia.Infrastructure/Data/VitaliaDbContext.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Vitalia.API.Data;
+namespace Vitalia.Infrastructure.Data;
 
 public class VitaliaDbContext : DbContext
 {
