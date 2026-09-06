2ac7828eee8553dac2e86d6442951233d10ca193
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 15:18:11 -0300

    feat(IOrderService): adicionado métodos de status

--- Vitalia.Application/Interfaces/Services/IOrderService.cs
+++ Vitalia.Application/Interfaces/Services/IOrderService.cs
@@ -5,8 +5,12 @@ namespace Vitalia.Application.Interfaces.Services;
 public interface IOrderService
 {
     Task<OrderResponse?> GetByIdAsync(long id);
-
     Task<IEnumerable<OrderResponse>> GetByUserIdAsync(long userId);
-
     Task<OrderResponse> CheckoutAsync(long cartId);
+
+    Task ConfirmAsync(long orderId);
+    Task ProcessAsync(long orderId);
+    Task ShipAsync(long orderId);
+    Task DeliverAsync(long orderId);
+    Task CancelAsync(long orderId);
 }
