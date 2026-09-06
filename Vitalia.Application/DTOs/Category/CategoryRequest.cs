6b2398448d046737bbb2786992c8f3bb4dc5e0be
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 17:12:04 -0300

    feat(CategoryRequest): feito validação do CategoryRequest

--- Vitalia.Application/DTOs/Category/CategoryRequest.cs
+++ Vitalia.Application/DTOs/Category/CategoryRequest.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Vitalia.Application.DTOs.Category;
 
 public class CategoryRequest
 {
+    [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+    [StringLength(
+        100,
+        ErrorMessage = "O nome da categoria deve ter no máximo 100 caracteres.")]
     public string Name { get; set; } = string.Empty;
 
+    [StringLength(
+        500,
+        ErrorMessage = "A descrição deve ter no máximo 500 caracteres.")]
     public string? Description { get; set; }
 }
