878d7df4c906abeca9f3631b660cd533d4f0562c
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 12:00:02 -0300

    feat(CategoryRequest): criado request CategoryRequest

---
+++ Vitalia.Application/DTOs/Category/CategoryRequest.cs
@@ -0,0 +1,8 @@
+namespace Vitalia.Application.DTOs.Category;
+
+public class CategoryRequest
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+}
---
+++ Vitalia.Application/DTOs/Category/CategoryResponse.cs
@@ -0,0 +1,6 @@
+namespace Vitalia.Application.DTOs.Category;
+
+public class CategoryResponse
+{
+    
+}
