4e593e73b2241f4e6609a916defe358814dc2ce8
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 14:24:03 -0300

    feat(OrderItemRepository): criado repository OrderItemRepository

--- Vitalia.Infrastructure/Repositories/OrderItemRepository.cs
+++ Vitalia.Infrastructure/Repositories/OrderItemRepository.cs
@@ -1,6 +1,20 @@
+using Vitalia.Application.Interfaces.Repositories;
+using Vitalia.Domain.Entities;
+using Vitalia.Infrastructure.Data;
+
 namespace Vitalia.Infrastructure.Repositories;
 
-public class OrderItemRepository
+public class OrderItemRepository : IOrderItemRepository
 {
-    
+    private readonly VitaliaDbContext _context;
+
+    public OrderItemRepository(VitaliaDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(OrderItem orderItem)
+    {
+        await _context.OrderItems.AddAsync(orderItem);
+    }
 }
