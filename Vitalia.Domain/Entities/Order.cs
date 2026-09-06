b1c6fd846809b4f7c7c720adfa006f78a59392cc
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 14:36:21 -0300

    feat(Order): criado construtor e seus métodos

--- Vitalia.Domain/Entities/Order.cs
+++ Vitalia.Domain/Entities/Order.cs
@@ -9,9 +9,53 @@ public class Order : Entity
 
     public DateTime OrderDate { get; private set; }
 
-    public OrderStatus Status { get; private set; } = OrderStatus.PENDING;
+    public OrderStatus Status { get; private set; }
 
     public decimal TotalAmount { get; private set; }
 
-    public ICollection<OrderItem> Items { get; private set; } = new List<OrderItem>();
+    public ICollection<OrderItem> Items { get; private set; }
+        = new List<OrderItem>();
+
+    public Order(long userId)
+    {
+        UserId = userId;
+        OrderDate = DateTime.UtcNow;
+        Status = OrderStatus.PENDING;
+        TotalAmount = 0;
+    }
+
+    public void AddItem(OrderItem item)
+    {
+        Items.Add(item);
+    }
+
+    public void SetTotalAmount(decimal totalAmount)
+    {
+        TotalAmount = totalAmount;
+    }
+
+    public void Confirm()
+    {
+        Status = OrderStatus.CONFIRMED;
+    }
+
+    public void Process()
+    {
+        Status = OrderStatus.PROCESSING;
+    }
+
+    public void Ship()
+    {
+        Status = OrderStatus.SHIPPED;
+    }
+
+    public void Deliver()
+    {
+        Status = OrderStatus.DELIVERED;
+    }
+
+    public void Cancel()
+    {
+        Status = OrderStatus.CANCELLED;
+    }
 }
