0463c848775d9abcd3f1348338f00ac946242075
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 08:13:12 -0300

    feat(IProductRepository): criado interface IProductRepository

---
+++ Vitalia.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+using Vitalia.Domain.Entities;
+
+namespace Vitalia.Application.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+
+    Task<Product?> GetByIdAsync(long id);
+
+    Task AddAsync(Product product);
+
+    void Update(Product product);
+
+    void Delete(Product product);
+
+    Task<bool> ExistsAsync(long id);
+}
