d5511ced96724baf57e9ff1ec2027b4d868b3562
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 13:37:42 -0300

    feat(Cart): criado construtor Cart
    E os métodos AddItem, RemoveItem, Checkout e Abandon

--- Vitalia.Domain/Entities/Cart.cs
+++ Vitalia.Domain/Entities/Cart.cs
@@ -7,12 +7,43 @@ public class Cart : Entity
 {
     public long UserId { get; private set; }
 
-    public CartStatus Status { get; private set; } = CartStatus.ACTIVE;
+    public CartStatus Status { get; private set; }
 
     public DateTime CreatedAt { get; private set; }
 
     public DateTime UpdatedAt { get; private set; }
-    
-    
+
     public ICollection<CartItem> Items { get; private set; } = new List<CartItem>();
+
+    public Cart(long userId)
+    {
+        UserId = userId;
+        Status = CartStatus.ACTIVE;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void AddItem(CartItem item)
+    {
+        Items.Add(item);
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void RemoveItem(CartItem item)
+    {
+        Items.Remove(item);
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Checkout()
+    {
+        Status = CartStatus.CHECKED_OUT;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Abandon()
+    {
+        Status = CartStatus.ABANDONED;
+        UpdatedAt = DateTime.UtcNow;
+    }
 }
