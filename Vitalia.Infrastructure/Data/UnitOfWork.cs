a0cabcc78a1702f2cfa12a66ce61d2306c9745b9
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 10:07:52 -0300

    feat(UnitOfWork): criado IUnitOfWork para abstrair acesso de dados

--- Vitalia.Infrastructure/Data/UnitOfWork.cs
+++ Vitalia.Infrastructure/Data/UnitOfWork.cs
@@ -1,6 +1,18 @@
+using Vitalia.Application.Interfaces.Repositories;
+
 namespace Vitalia.Infrastructure.Data;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
-    
+    private readonly VitaliaDbContext _context;
+
+    public UnitOfWork(VitaliaDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
 }
