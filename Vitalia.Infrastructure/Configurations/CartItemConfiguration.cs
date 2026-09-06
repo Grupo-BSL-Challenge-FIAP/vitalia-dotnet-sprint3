137f578b8a5bd31ed688ced4de9001edfa9ad789
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-05 23:11:38 -0300

    feat(CartItemConfiguration): mapeado CartItem no CartItemConfiguration

--- Vitalia.Infrastructure/Configurations/CartItemConfiguration.cs
+++ Vitalia.Infrastructure/Configurations/CartItemConfiguration.cs
@@ -1,6 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vitalia.Domain.Entities;
+
 namespace Vitalia.Infrastructure.Configurations;
 
-public class CartItemConfiguration
+public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
 {
-    
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.ToTable("TB_VITALIA_CART_ITEM");
+
+        builder.HasKey(ci => ci.Id);
+
+        builder.Property(ci => ci.Id)
+            .HasColumnName("CART_ITEM_ID");
+
+        builder.Property(ci => ci.CartId)
+            .HasColumnName("CART_ID")
+            .IsRequired();
+
+        builder.Property(ci => ci.ProductId)
+            .HasColumnName("PRODUCT_ID")
+            .IsRequired();
+
+        builder.Property(ci => ci.Quantity)
+            .HasColumnName("QUANTITY")
+            .IsRequired();
+
+        builder.Property(ci => ci.UnitPrice)
+            .HasColumnName("UNIT_PRICE")
+            .HasPrecision(12, 2)
+            .IsRequired();
+
+        builder.HasOne(ci => ci.Cart)
+            .WithMany(c => c.Items)
+            .HasForeignKey(ci => ci.CartId);
+
+        builder.HasOne(ci => ci.Product)
+            .WithMany()
+            .HasForeignKey(ci => ci.ProductId);
+
+        builder.HasIndex(ci => new { ci.CartId, ci.ProductId })
+            .IsUnique();
+    }
 }
