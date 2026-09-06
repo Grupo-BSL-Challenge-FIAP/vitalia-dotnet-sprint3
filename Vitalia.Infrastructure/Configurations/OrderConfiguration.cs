841326818e4ec14b3217cc0a2d3033960a5690c5
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-05 23:18:11 -0300

    feat(OrderConfiguration): mapeado Order no OrderConfiguration

--- Vitalia.Infrastructure/Configurations/OrderConfiguration.cs
+++ Vitalia.Infrastructure/Configurations/OrderConfiguration.cs
@@ -1,6 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vitalia.Domain.Entities;
+
 namespace Vitalia.Infrastructure.Configurations;
 
-public class OrderConfiguration
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
-    
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("TB_VITALIA_ORDERS");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id)
+            .HasColumnName("ORDER_ID");
+
+        builder.Property(o => o.UserId)
+            .HasColumnName("USER_ID")
+            .IsRequired();
+
+        builder.Property(o => o.OrderDate)
+            .HasColumnName("ORDER_DATE")
+            .IsRequired();
+
+        builder.Property(o => o.Status)
+            .HasColumnName("STATUS")
+            .HasMaxLength(30)
+            .IsRequired()
+            .HasConversion<string>();
+
+        builder.Property(o => o.TotalAmount)
+            .HasColumnName("TOTAL_AMOUNT")
+            .HasPrecision(12, 2)
+            .IsRequired();
+
+        builder.HasMany(o => o.Items)
+            .WithOne(oi => oi.Order)
+            .HasForeignKey(oi => oi.OrderId);
+    }
 }
