6b7b2967966d7e3a3e5acfe9c093a3e47f3d78be
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 14:00:12 -0300

    feat(ICartService): criado interface ICartService

--- Vitalia.Application/Interfaces/Services/ICartService.cs
+++ Vitalia.Application/Interfaces/Services/ICartService.cs
@@ -1,6 +1,25 @@
+using Vitalia.Application.DTOs.Cart;
+
 namespace Vitalia.Application.Interfaces.Services;
 
-public class ICartService
+public interface ICartService
 {
-    
+    Task<CartResponse> GetOrCreateActiveCartAsync(long userId);
+
+    Task<CartResponse?> GetByIdAsync(long id);
+
+    Task<CartResponse> AddItemAsync(
+        long cartId,
+        CartItemRequest request);
+
+    Task<CartResponse> UpdateItemAsync(
+        long cartId,
+        long productId,
+        int quantity);
+
+    Task RemoveItemAsync(
+        long cartId,
+        long productId);
+
+    Task CheckoutAsync(long cartId);
 }
