9803b57b0ba912b57da3349bb188a5c1a8848ff5
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 10:02:58 -0300

    feat(ProductRequest): criado request ProductRequest

--- Vitalia.Application/DTOs/Product/ProductRequest.cs
+++ Vitalia.Application/DTOs/Product/ProductRequest.cs
@@ -2,5 +2,13 @@ namespace Vitalia.Application.DTOs.Product;
 
 public class ProductRequest
 {
-    
+    public long CategoryId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; }
 }
