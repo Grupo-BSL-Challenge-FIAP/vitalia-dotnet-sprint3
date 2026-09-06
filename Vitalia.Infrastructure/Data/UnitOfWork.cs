abf0846fad221ba0620c198fcb2191eae915c019
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 18:20:54 -0300

    feat(UnitOfWork): implementado métodos BeginTransactionAsync CommitTransactionAsync RollbackTransactionAsync

--- Vitalia.Infrastructure/Data/UnitOfWork.cs
+++ Vitalia.Infrastructure/Data/UnitOfWork.cs
@@ -1,4 +1,5 @@
 using Vitalia.Application.Interfaces.Repositories;
+using Vitalia.Infrastructure.Data;
 
 namespace Vitalia.Infrastructure.Data;
 
@@ -14,5 +15,26 @@ public class UnitOfWork : IUnitOfWork
     public async Task<int> SaveChangesAsync()
     {
         return await _context.SaveChangesAsync();
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+        if (_context.Database.CurrentTransaction is not null)
+        {
+            await _context.Database.CurrentTransaction.CommitAsync();
+        }
+    }
+
+    public async Task RollbackTransactionAsync()
+    {
+        if (_context.Database.CurrentTransaction is not null)
+        {
+            await _context.Database.CurrentTransaction.RollbackAsync();
+        }
     }
 }
