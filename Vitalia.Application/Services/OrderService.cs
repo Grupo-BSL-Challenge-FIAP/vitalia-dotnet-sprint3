16ab79a246c54936b00b16d6e981c32b1d5b76a6
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 14:33:07 -0300

    feat(OrderService): criado service OrderService

--- Vitalia.Application/Services/OrderService.cs
+++ Vitalia.Application/Services/OrderService.cs
@@ -1,6 +1,40 @@
+using Vitalia.Application.DTOs.Order;
+using Vitalia.Application.Interfaces.Repositories;
+using Vitalia.Application.Interfaces.Services;
+
 namespace Vitalia.Application.Services;
 
-public class OrderService
+public class OrderService : IOrderService
 {
-    
+    private readonly IOrderRepository _orderRepository;
+    private readonly ICartRepository _cartRepository;
+    private readonly IProductRepository _productRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public OrderService(
+        IOrderRepository orderRepository,
+        ICartRepository cartRepository,
+        IProductRepository productRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _orderRepository = orderRepository;
+        _cartRepository = cartRepository;
+        _productRepository = productRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<OrderResponse?> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<OrderResponse>> GetByUserIdAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<OrderResponse> CheckoutAsync(long cartId)
+    {
+        throw new NotImplementedException();
+    }
 }
