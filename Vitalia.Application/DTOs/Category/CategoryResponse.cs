ba1a4dda23c9b4a1b5ded908a48d4ef5632d56d8
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 12:00:34 -0300

    feat(CategoryResponse): criado response CategoryResponse

--- Vitalia.Application/DTOs/Category/CategoryResponse.cs
+++ Vitalia.Application/DTOs/Category/CategoryResponse.cs
@@ -2,5 +2,9 @@ namespace Vitalia.Application.DTOs.Category;
 
 public class CategoryResponse
 {
-    
+    public long Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
 }
