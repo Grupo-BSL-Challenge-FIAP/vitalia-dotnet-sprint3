1ecd7a2194e16850a9437d81462950054817443f
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 08:33:55 -0300

    feat(ProductService): criado service ProductService

--- Vitalia.Application/Services/ProductService.cs
+++ Vitalia.Application/Services/ProductService.cs
@@ -1,6 +1,56 @@
+using Vitalia.Application.Interfaces.Repositories;
+using Vitalia.Application.Interfaces.Services;
+using Vitalia.Domain.Entities;
+
 namespace Vitalia.Application.Services;
 
-public class ProductService
+public class ProductService : IProductService
 {
-    
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return await _productRepository.GetAllAsync();
+    }
+
+    public async Task<Product?> GetByIdAsync(long id)
+    {
+        return await _productRepository.GetByIdAsync(id);
+    }
+
+    public async Task AddAsync(Product product)
+    {
+        await _productRepository.AddAsync(product);
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        if (!await _productRepository.ExistsAsync(product.Id))
+        {
+            throw new KeyNotFoundException(
+                $"Produto com ID {product.Id} não encontrado."
+            );
+        }
+
+        _productRepository.Update(product);
+    }
+
+    public async Task DeleteAsync(long id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+
+        if (product is null)
+        {
+            throw new KeyNotFoundException(
+                $"Produto com ID {id} não encontrado."
+            );
+        }
+
+        _productRepository.Delete(product);
+    }
 }
