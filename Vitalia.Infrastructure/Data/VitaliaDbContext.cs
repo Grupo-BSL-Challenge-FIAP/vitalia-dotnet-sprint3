64ffa45e0c06d339ac5fe242177d815fc766d916
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 13:40:10 -0300

    feat(VitaliaDbContext): criado DbSet CartItems

--- Vitalia.Infrastructure/Data/VitaliaDbContext.cs
+++ Vitalia.Infrastructure/Data/VitaliaDbContext.cs
@@ -15,6 +15,8 @@ public class VitaliaDbContext : DbContext
     public DbSet<Product> Products { get; set; }
     
     public DbSet<Cart> Carts { get; set; }
+    
+    public DbSet<CartItem> CartItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
