4d937002bc24b24d74518ad5e3762d3cb18f0858
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 13:04:18 -0300

    feat(Program): adicionado repository CartRepository

--- Vitalia.API/Program.cs
+++ Vitalia.API/Program.cs
@@ -42,9 +42,13 @@ builder.Services.AddDbContext<VitaliaDbContext>(options =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
