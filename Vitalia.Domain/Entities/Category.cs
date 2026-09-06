8326e381c65ffb8cdd270d8a63f4b1d0ac170092
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-05 22:20:59 -0300

    feat(Category): criado entidade Category

--- Vitalia.Domain/Entities/Category.cs
+++ Vitalia.Domain/Entities/Category.cs
@@ -1,6 +1,10 @@
+using Vitalia.Domain.Common;
+
 namespace Vitalia.Domain.Entities;
 
-public class Category
+public class Category : Entity
 {
-    
+    public string Name { get; private set; } = String.Empty;
+
+    public string? Description { get; private set; }
 }
