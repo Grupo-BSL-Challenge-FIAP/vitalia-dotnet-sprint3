142b18c90054c32614d65964a8f56f72cff81bd2
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-05 23:17:31 -0300

    feat(Order): criado entidade Order

--- Vitalia.Domain/Entities/Order.cs
+++ Vitalia.Domain/Entities/Order.cs
@@ -1,6 +1,17 @@
+using Vitalia.Domain.Common;
+using Vitalia.Domain.Enums;
+
 namespace Vitalia.Domain.Entities;
 
-public class Order
+public class Order : Entity
 {
-    
+    public long UserId { get; private set; }
+
+    public DateTime OrderDate { get; private set; }
+
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalAmount { get; private set; }
+
+    public ICollection<OrderItem> Items { get; private set; } = new List<OrderItem>();
 }
