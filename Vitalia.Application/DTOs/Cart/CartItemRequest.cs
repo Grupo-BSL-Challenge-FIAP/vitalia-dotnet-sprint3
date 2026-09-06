edf7567e8988507705a7e2dda783d74c6ebbbfbc
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 17:52:04 -0300

    feat(CartItemRequest): criado validação no CartItemRequest

--- Vitalia.Application/DTOs/Cart/CartItemRequest.cs
+++ Vitalia.Application/DTOs/Cart/CartItemRequest.cs
@@ -1,8 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Vitalia.Application.DTOs.Cart;
 
 public class CartItemRequest
 {
+    [Range(
+        1,
+        long.MaxValue,
+        ErrorMessage = "O produto deve ser informado."
+    )]
     public long ProductId { get; set; }
 
+    [Range(
+        1,
+        int.MaxValue,
+        ErrorMessage = "A quantidade deve ser maior que zero."
+    )]
     public int Quantity { get; set; }
 }
