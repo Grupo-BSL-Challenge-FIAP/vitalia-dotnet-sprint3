80cba8bb2256ba0c10a9160b65c04b7ad42e3fa0
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 17:06:13 -0300

    feat(ProductPagedResponse): criado response ProductPagedResponse

--- Vitalia.Application/DTOs/Product/ProductPagedResponse.cs
+++ Vitalia.Application/DTOs/Product/ProductPagedResponse.cs
@@ -2,5 +2,13 @@ namespace Vitalia.Application.DTOs.Product;
 
 public class ProductPagedResponse
 {
-    
+    public IEnumerable<ProductResponse> Items { get; set; } = [];
+
+    public int Page { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int TotalItems { get; set; }
+
+    public int TotalPages { get; set; }
 }
