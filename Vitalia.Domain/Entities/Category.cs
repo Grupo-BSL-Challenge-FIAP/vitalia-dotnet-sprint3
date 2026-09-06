25d3aa075d9a9425fbd281db33814ae24d0f67de
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 12:39:33 -0300

    feat(Category): criado contrutor Category e método Update

--- Vitalia.Domain/Entities/Category.cs
+++ Vitalia.Domain/Entities/Category.cs
@@ -4,7 +4,23 @@ namespace Vitalia.Domain.Entities;
 
 public class Category : Entity
 {
-    public string Name { get; private set; } = String.Empty;
+    public string Name { get; private set; } = string.Empty;
 
     public string? Description { get; private set; }
+
+    public Category(
+        string name,
+        string? description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public void Update(
+        string name,
+        string? description)
+    {
+        Name = name;
+        Description = description;
+    }
 }
