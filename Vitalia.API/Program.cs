21ba40b4f319b2060c5242f9795704038902f545
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 11:41:59 -0300

    feat(Program): adicionado title, version e description no Swagger

--- Vitalia.API/Program.cs
+++ Vitalia.API/Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 using Vitalia.Infrastructure.Data;
 using Vitalia.Application.Interfaces.Repositories;
 using Vitalia.Application.Interfaces.Services;
@@ -9,7 +11,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Vitalia",
+        Version = "v1",
+        Description =
+            "API da plataforma Vitalia para gerenciamento de produtos, pets,"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath);
+    }
+});
 
 var connectionString = builder.Configuration.GetConnectionString("OracleConnection")
                        ?? throw new InvalidOperationException(
@@ -27,9 +48,17 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/swagger/v1/swagger.json",
+            "Vitalia v1"
+        );
+
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
