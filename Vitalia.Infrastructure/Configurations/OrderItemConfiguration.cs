d0dbc0384ea1b0c298f0e9f902892379485319ae
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-05 23:16:11 -0300

    feat(OrderItemConfiguration): mapeado OrderItem no OrderItemConfiguration

--- Vitalia.Infrastructure/Configurations/OrderItemConfiguration.cs
+++ Vitalia.Infrastructure/Configurations/OrderItemConfiguration.cs
@@ -1,6 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vitalia.Domain.Entities;
+
 namespace Vitalia.Infrastructure.Configurations;
 
-public class OrderItemConfiguration
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
 {
-    
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.ToTable("TB_VITALIA_ORDER_ITEM");
+
+        builder.HasKey(oi => oi.Id);
+
+        builder.Property(oi => oi.Id)
+            .HasColumnName("ORDER_ITEM_ID");
+
+        builder.Property(oi => oi.OrderId)
+            .HasColumnName("ORDER_ID")
+            .IsRequired();
+
+        builder.Property(oi => oi.ProductId)
+            .HasColumnName("PRODUCT_ID")
+            .IsRequired();
+
+        builder.Property(oi => oi.Quantity)
+            .HasColumnName("QUANTITY")
+            .IsRequired();
+
+        builder.Property(oi => oi.UnitPrice)
+            .HasColumnName("UNIT_PRICE")
+            .HasPrecision(12, 2)
+            .IsRequired();
+
+        builder.Property(oi => oi.Subtotal)
+            .HasColumnName("SUBTOTAL")
+            .HasPrecision(12, 2)
+            .IsRequired();
+
+        builder.HasOne(oi => oi.Order)
+            .WithMany(o => o.Items)
+            .HasForeignKey(oi => oi.OrderId);
+
+        builder.HasOne(oi => oi.Product)
+            .WithMany()
+            .HasForeignKey(oi => oi.ProductId);
+    }
 }
