3ac608fe41375d0bc0f3ca326ae70776ee30d422
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 13:55:07 -0300

    feat(CartResponse): criado response CartResponse

--- Vitalia.Application/DTOs/Cart/CartResponse.cs
+++ Vitalia.Application/DTOs/Cart/CartResponse.cs
@@ -2,5 +2,18 @@ namespace Vitalia.Application.DTOs.Cart;
 
 public class CartResponse
 {
-    
+    public long Id { get; set; }
+
+    public long UserId { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public decimal Total { get; set; }
+
+    public IEnumerable<CartItemResponse> Items { get; set; }
+        = new List<CartItemResponse>();
 }
