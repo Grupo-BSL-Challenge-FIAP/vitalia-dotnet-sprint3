856c47588781176fb7ccfca7778d6294ed7f78c8
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 22:32:22 -0300

    feat(JwtConfiguration): feito configuração do JWT
    Ele vêm do java e a chave está guardada no user-secrets

---
+++ Vitalia.API/Configurations/JwtConfiguration.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Vitalia.API.Configurations;
+
+public static class JwtConfiguration
+{
+    public const string Issuer = "vitalia-api";
+
+    public static void Configure(
+        IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var jwtSecret = configuration["JWT_SECRET"]
+                        ?? throw new InvalidOperationException(
+                            "A variável JWT_SECRET não foi encontrada."
+                        );
+
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = Issuer,
+
+                    ValidateAudience = false,
+
+                    ValidateLifetime = true,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSecret)
+                    )
+                };
+            });
+
+        services.AddAuthorization();
+    }
+}
