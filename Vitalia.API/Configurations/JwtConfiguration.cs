1b3d1800fe4723e21b49cdf6a699d0c5bc7f3d24
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-07 02:31:18 -0300

    feat(JwtConfiguration): adicionado MapInboundClaims como false

--- Vitalia.API/Configurations/JwtConfiguration.cs
+++ Vitalia.API/Configurations/JwtConfiguration.cs
@@ -21,6 +21,8 @@ public static class JwtConfiguration
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
+                options.MapInboundClaims = false;
+
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
