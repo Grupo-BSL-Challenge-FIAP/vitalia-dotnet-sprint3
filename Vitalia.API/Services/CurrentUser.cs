06e93587f3a77095bf81607bdb16e32b95db463e
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-06 23:32:03 -0300

    feat(CurrentUser): criado service CurrentUser

--- Vitalia.API/Services/CurrentUser.cs
+++ Vitalia.API/Services/CurrentUser.cs
@@ -1,6 +1,35 @@
+using Vitalia.Application.Interfaces.Services;
+
 namespace Vitalia.API.Services;
 
-public class CurrentUser
+public class CurrentUser(IHttpContextAccessor httpContextAccessor)
+    : ICurrentUser
 {
-    
+    public long UserId
+    {
+        get
+        {
+            var userIdClaim = httpContextAccessor
+                .HttpContext?
+                .User
+                .FindFirst("userId")?
+                .Value;
+
+            if (string.IsNullOrWhiteSpace(userIdClaim))
+            {
+                throw new UnauthorizedAccessException(
+                    "O usuário autenticado não possui um userId válido."
+                );
+            }
+
+            if (!long.TryParse(userIdClaim, out var userId))
+            {
+                throw new UnauthorizedAccessException(
+                    "O userId do token JWT é inválido."
+                );
+            }
+
+            return userId;
+        }
+    }
 }
