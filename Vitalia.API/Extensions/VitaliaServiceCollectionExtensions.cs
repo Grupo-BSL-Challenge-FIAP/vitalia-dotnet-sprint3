137439f8276a01ae388e3e9d41f9e388a294a1b7
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-07 10:19:21 -0300

    feat(VitaliaServiceCollectionExtensions): criado VitaliaServiceCollectionExtensions

--- Vitalia.API/Extensions/VitaliaServiceCollectionExtensions.cs
+++ Vitalia.API/Extensions/VitaliaServiceCollectionExtensions.cs
@@ -1,6 +1,35 @@
+using Vitalia.Application.Interfaces.Repositories;
+using Vitalia.Application.Interfaces.Services;
+using Vitalia.Application.Services;
+using Vitalia.API.Services;
+using Vitalia.Infrastructure.Data;
+using Vitalia.Infrastructure.Repositories;
+
 namespace Vitalia.API.Extensions;
 
-public class VitaliaServiceCollectionExtensions
+public static class VitaliaServiceCollectionExtensions
 {
-    
+    public static IServiceCollection AddVitaliaServices(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IProductService, ProductService>();
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<ICategoryService, CategoryService>();
+
+        services.AddScoped<ICartRepository, CartRepository>();
+        services.AddScoped<ICartItemRepository, CartItemRepository>();
+        services.AddScoped<ICartService, CartService>();
+
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+        services.AddScoped<IOrderService, OrderService>();
+
+        services.AddScoped<ICurrentUser, CurrentUser>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
 }
